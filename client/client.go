fff4cab41900438b36a22be7ef2a2208f8a92ec6
Michael Thaler <michael.thaler@dlr.de>
2021-05-18 10:36:42 +0200

    Added pasqueflower.jpg.

--- client/client.go
+++ client/client.go
@@ -1,5 +1,129 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"image"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func imageResizeRPC(img image.Image) (res int, err error) {
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatalln("Usage: server user password")
+	}
+
+	user := args[1]
+	password := args[2]
+
+	url := fmt.Sprintf("amqp://%s:%s@localhost:5672/image-resizer", user, password)
+	fmt.Printf("URL: %s", url)
+
+	fmt.Println("Connecting to RabbitMQ broker...")
+	conn, err := amqp.Dial(url)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	fmt.Println("Connected to RabbitMQ broker")
+	defer conn.Close()
+
+	fmt.Println("Opening channel...")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	fmt.Println("Opened channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"image-resizer",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	fmt.Println("Declaring queue...")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	fmt.Println("Declared queue")
+
+	corrId := randomString(32)
+
+	err = ch.Publish(
+		"",          // exchange
+		"image-resizer", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(strconv.Itoa(n)),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			res, err = strconv.Atoi(string(d.Body))
+			failOnError(err, "Failed to convert body to integer")
+			break
+		}
+	}
+
+	return
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := bodyFrom(os.Args)
+
+	log.Printf(" [x] Requesting fib(%d)", n)
+	res, err := fibonacciRPC(n)
+	failOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %d", res)
+}
+
+func bodyFrom(args []string) int {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "30"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	n, err := strconv.Atoi(s)
+	failOnError(err, "Failed to convert arg to integer")
+	return n
 }
